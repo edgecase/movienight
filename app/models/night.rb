222a91e70c347e7d855c028c005e47e7f7f25635
Jon Distad <jon@theedgecase.com>
2009-08-22 12:57:41 -0400

    invitee salt is generated before create on nights

--- app/models/night.rb
+++ app/models/night.rb
@@ -1,8 +1,18 @@
 class Night < ActiveRecord::Base
+  extend Authentication::ModelClassMethods
+
   belongs_to :host, :class_name => "User"
   belongs_to :location
 
   validates_presence_of :doors_open_date
   validates_presence_of :doors_open_time
   validates_presence_of :host
+
+  before_create :generate_invitee_salt
+
+  private
+
+  def generate_invitee_salt
+    self.invitee_salt = Night.make_token
+  end
 end
