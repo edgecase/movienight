8f78e52f7906ae786dc6b13b488398cb31e7a6ed
Ehren Murdick <ehren@theedgecase.com>
2009-08-22 11:37:39 -0400

    updated night display

--- app/models/night.rb
+++ app/models/night.rb
@@ -1,2 +1,9 @@
 class Night < ActiveRecord::Base
+  def curtain_time_at
+    "#{curtain_date.try(:to_s, :date)}, #{curtain_time.try(:to_s, :time)}"
+  end
+
+  def doors_open_at
+    "#{doors_open_date.try(:to_s, :date)}, #{doors_open_time.try(:to_s, :time)}"
+  end
 end
