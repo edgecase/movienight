aa9c9a5bb227bfedaf946424e8231107fccd31a6
Ehren Murdick <ehren@theedgecase.com>
2009-08-22 13:44:54 -0400

    added some specs to location

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,10 +1,30 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.dirname(__FILE__) + '/../spec_controller_helper'
 
 describe LocationsController do
+  describe "#create" do
+    it_should_behave_like "an action that requires login"
+    before do
+      @location = Factory(:location)
+      Location.stub!(:new).and_return(@location)
+    end
+    def http_request
+      post :create, :location => {:some => :params}
+    end
+    
+    describe "with valid params" do
+      it "should render the list of locations" do
+        http_request
+        assigns[:location].should == @location
+        response.should render_template("index")
+      end
+    end
 
-  #Delete this example and add some real ones
-  it "should use LocationsController" do
-    controller.should be_an_instance_of(LocationsController)
+    describe "with invalid params" do
+      it "should render the list of locations" do
+        @location.stub!(:save).and_return(false)
+        http_request
+        response.should_not be_success
+      end
+    end
   end
-
 end
