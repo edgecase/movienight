28c576c4c37cfecdccaa3324ca646e0c5b65f055
Jon Distad <jon@theedgecase.com>
2009-08-22 12:44:27 -0400

    rename hash to access_hash on invitees

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -1,15 +1,16 @@
 class Invitee < ActiveRecord::Base
   include Authentication
-  include Authentication::ByPassword
 
   belongs_to :night
 
-  validates_presence_of :night, :email, :hash
+  validates_presence_of :night, :email, :access_hash
 
   validates_uniqueness_of :email
   validates_format_of     :email, :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  before_validation :generate_hash
+  before_create :generate_access_hash
 
-  
+  def generate_access_hash
+    
+  end
 end
