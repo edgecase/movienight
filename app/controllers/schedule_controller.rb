d9e70491c0ca1c246aad14cf4d43863b9bd091bd
Jon Distad <jon@theedgecase.com>
2009-08-22 00:25:23 -0400

    make schedules controller plural

--- app/controllers/schedule_controller.rb
+++
@@ -1,4 +0,0 @@
-class ScheduleController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'schedule', :action => 'index'
+  map.root :controller => 'schedules', :action => 'index'
 
   map.logout   '/logout',   :controller => 'sessions', :action => 'destroy'
   map.login    '/login',    :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users',    :action => 'create'
   map.signup   '/signup',   :controller => 'users',    :action => 'new'
   map.resources :users
-  map.resource :session
+  map.resources :session
 
-  map.resource :schedule
+  map.resources :schedules
   map.resources :nights
 
   map.connect ':controller/:action/:id'
