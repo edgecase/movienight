b5afa6ad3e93dd3e883755666e025e6062cc079f
Jerry Nummi <jerry@theedgecase.com>
2009-08-22 12:57:58 -0400

    Added JS to open new windows for external links.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,9 @@
 $(function() {
   $('[rel=date]').datepicker();
+
+  $('a[rel=external]').click(function() {
+    var newWindow = window.open($(this).attr('href'), '_blank');
+    if(newWindow) { if(newWindow.focus) { newWindow.focus(); return false; } }
+    newWindow = null;
+  });
 });
