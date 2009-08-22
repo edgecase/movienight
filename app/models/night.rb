c459f46af62e9d3f247cc1a14f750ab07fa3b0c0
Ehren Murdick <ehren@theedgecase.com>
2009-08-22 12:07:20 -0400

    time_at methods are naow unused!

--- app/models/night.rb
+++ app/models/night.rb
@@ -4,12 +4,4 @@ class Night < ActiveRecord::Base
   validates_presence_of :doors_open_date
   validates_presence_of :doors_open_time
   validates_presence_of :host
-
-  def curtain_time_at
-    "#{curtain_date.try(:to_s, :date)}, #{curtain_time.try(:to_s, :time)}"
-  end
-
-  def doors_open_at
-    "#{doors_open_date.try(:to_s, :date)}, #{doors_open_time.try(:to_s, :time)}"
-  end
 end
--- spec/models/night_spec.rb
+++ spec/models/night_spec.rb
@@ -1,18 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Night do
-  before do
-    User.destroy_all
-  end
-
   it "should create a new instance given valid attributes" do
     Factory(:night).save.should == true
-  end
-
-  it "should compose doors_open_at from date and time" do
-    time = Time.now
-    date = Date.today
-    night = Factory(:night, :doors_open_time => time, :doors_open_date => date)
-    night.doors_open_at.should == "#{date.to_s(:date)}, #{time.to_s(:time)}"
   end
 end
