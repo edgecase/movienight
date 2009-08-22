6616b2bfe3e1e8f4fb4a94a2d57eeca61972e267
Jon Distad <jon@theedgecase.com>
2009-08-22 18:25:37 -0400

    nights set up to send invitations

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -4,12 +4,14 @@ class Invitee < ActiveRecord::Base
 
   belongs_to :night
 
-  validates_presence_of :night_id, :email
+  validates_presence_of :night, :email
 
   validates_uniqueness_of :email
   validates_format_of     :email, :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   before_create :generate_access_hash
+
+  private
 
   def generate_access_hash
     self.access_hash = Invitee.password_digest(email, night.invitee_salt)
