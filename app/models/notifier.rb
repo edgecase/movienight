c667a2f816ae25fa2c177a5e641b3e06ceb094b6
Jon Distad <jon@theedgecase.com>
2009-08-23 15:38:05 -0400

    fixed member invitation mailer method name

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,9 +1,9 @@
 class Notifier < ActionMailer::Base
   
-  def registered_member_invitiation(user, night)
+  def registered_member_invitation(user, night)
     subject "You have been invited to a movie night!"
     recipients user.email
-    from "no-reply@example.com"
+    from "MovieNight <no-reply@example.com>"
     content_type 'text/html'
     body :night => night
   end
@@ -11,7 +11,7 @@ class Notifier < ActionMailer::Base
   def nonmember_invitation(invitee, night)
     subject "You have been invited to a movie night!"
     recipients invitee.email
-    from "no-reply@example.com"
+    from "MovieNight <no-reply@example.com>"
     content_type 'text/html'
     body :invitee => invitee, :night => night
   end
