493eeb812de07e4b151cc14a608803fed346b428
Tony Schneider + Matt Yoho <pairing+ts+my@edgecase.com>
2010-08-11 07:42:03 -0400

    Update RSpec to 1.3.0 (rspec-rails to 1.3.2)
    Also beginning use of FakeWeb but still allowing the call to the
    Movie Database through for now.

--- spec/lib/the_movie_database_spec.rb
+++ spec/lib/the_movie_database_spec.rb
@@ -1,6 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TheMovieDatabase do
+  before(:all) { FakeWeb.allow_net_connect = true  }
+  after (:all) { FakeWeb.allow_net_connect = false }
+
   it "should return potential matches for a title search" do
     matches = TheMovieDatabase.search_titles("Ghostbusters")
     matches.should_not be_nil
