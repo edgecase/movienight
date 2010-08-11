2fc2371a85be42843639dd756a6cc20ca7f50a47
Tony Schneider + Matt Yoho <pairing+ts+my@edgecase.com>
2010-08-11 08:02:48 -0400

    Refactor TheMovieDatabase module

--- lib/the_movie_database.rb
+++ lib/the_movie_database.rb
@@ -7,23 +7,35 @@ module TheMovieDatabase
   default_params :api_key => '2a9160b01325c41b4596f52a2b460842'
 
   def self.search_titles(title)
-    results = self.get('/Movie.search', :query => {:title => title})
-    raw_matches = results['results']['moviematches']['movie'] rescue []
-    search_results = []
-    raw_matches.each do |match|
-      if match['type'] == 'movie'
-        movie = Movie.new(
-          :title       => match['title'],
-          :alt_title   => match['alternative_title'],
-          :popularity  => match['popularity'],
-          :tmdb_id     => match['id'],
-          :imdb_id     => match['imdb'],
-          :posters     => match['poster']
-        )
-        movie.release = DateTime.parse(match['release']) if match['release'] 
-        search_results << movie
+    movie_listings_for(title).collect do |listing|
+      movie_from listing
+    end
+  end
+
+  class << self
+    private
+    def movie_listings_for(title)
+      begin
+        listings = get('/Movie.search', :query => {:title => title})
+        listings['results']['moviematches']['movie'].select do |listing|
+          listing['type'] == 'movie'
+        end
+      rescue
+        []
       end
     end
-    search_results
+
+    def movie_from(listing)
+      Movie.new(
+        :title      => listing['title'],
+        :alt_title  => listing['alternative_title'],
+        :popularity => listing['popularity'],
+        :tmdb_id    => listing['id'],
+        :imdb_id    => listing['imdb'],
+        :posters    => listing['poster']
+      ).tap do |movie|
+        movie.release = DateTime.parse(listing['release']) if listing['release'] 
+      end
+    end
   end
 end
