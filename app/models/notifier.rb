470cde6f18fff52473989e8937c962f8e49cb138
Matt Yoho <mby@mattyoho.com>
2010-11-02 16:09:56 -0400

    Missing notifier template variable

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,16 +1,16 @@
 class Notifier < ActionMailer::Base
 
-  default :from "invites@movienightapp.com"
+  default :from => "invites@movienightapp.com"
 
   def registered_member_invitation(user, night)
-    header[:content_type] = 'text/html'
-    @night = night
+    headers[:content_type] = 'text/html'
+    @user, @night = user, night
     mail :to => user.email, :subject => "You have been invited to a movie night!"
   end
 
   def nonmember_invitation(invitee, night)
-    header[:content_type] = 'text/html'
-    @night = night
+    headers[:content_type] = 'text/html'
+    @invitee, @night = invitee, night
     mail :to => invitee.email, :subject => "You have been invited to a movie night!"
   end
 
