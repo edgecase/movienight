c5fb6046dfc0c91c924864b641b9dcd22d087172
Matt Yoho <mby@mattyoho.com>
2010-11-02 14:39:21 -0400

    Clean out spec cruft

--- spec/lib/the_movie_database_spec.rb
+++ spec/lib/the_movie_database_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe TheMovieDatabase do
   before(:all) { FakeWeb.allow_net_connect = true  }
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,13 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe Vote do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Vote.create!(@valid_attributes)
-  end
 end
