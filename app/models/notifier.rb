f79cc641ac1a2ccdceece4ec49f63de5a1d104fa
Matt Yoho <mby@mattyoho.com>
2010-11-04 22:16:51 -0400

    DRYness

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,17 +1,18 @@
 class Notifier < ActionMailer::Base
 
-  default :from => "invites@movienightapp.com"
+  default :from    => "invites@movienightapp.com",
+          :subject => "You have been invited to a movie night!"
 
   def registered_member_invitation(user, night)
     headers[:content_type] = 'text/html'
     @user, @night = user, night
-    mail :to => user.email, :subject => "You have been invited to a movie night!"
+    mail :to => user.email
   end
 
   def nonmember_invitation(invitee, night)
     headers[:content_type] = 'text/html'
     @invitee, @night = invitee, night
-    mail :to => invitee.email, :subject => "You have been invited to a movie night!"
+    mail :to => invitee.email
   end
 
 end
